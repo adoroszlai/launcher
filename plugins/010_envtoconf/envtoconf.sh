d86213b1ed9578890836f733b02873babd4075dd
Doroszlai, Attila <adoroszlai@apache.org>
2018-02-17 16:01:48 +0100

    envtoconf plugin: create CONF_DIR if needed

--- plugins/010_envtoconf/envtoconf.sh
+++ plugins/010_envtoconf/envtoconf.sh
@@ -2,7 +2,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if [ "true" == "$ENVTOCONF_ENABLED" ] || [ "$CONFIG_TYPE" == "simple" ]; then
    plugin-is-active "ENVTOCONF"
-   $DIR/envtoconf --outputdir $CONF_DIR $@
+   [[ -n $CONF_DIR ]] && mkdir -p "$CONF_DIR"
+   $DIR/envtoconf --outputdir "$CONF_DIR" $@
 fi
 
 call-next-plugin "$@"
