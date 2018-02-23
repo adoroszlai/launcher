46dd674907a2eddf8d1ef7f335ea2d2ea9978823
Doroszlai, Attila <adoroszlai@apache.org>
2018-02-23 20:28:39 +0100

    Configurable envtoconf download URL

--- plugins/010_envtoconf/onbuild.sh
+++ plugins/010_envtoconf/onbuild.sh
@@ -1,4 +1,5 @@
 #/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-wget https://github.com/elek/envtoconf/releases/download/1.1.1/linux_amd64_envtoconf -O $DIR/envtoconf
+: ${ENVTOCONF_URL:=https://github.com/elek/envtoconf/releases/download/1.1.1/linux_amd64_envtoconf}
+wget "$ENVTOCONF_URL" -O $DIR/envtoconf
 chmod +x $DIR/envtoconf
